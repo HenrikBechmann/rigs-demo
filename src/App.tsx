6e9b7caf44cda0c02894269aaf45db7c1a3bcb88
Henrik Bechmann <henrik@therefore.ca>
2022-10-15 20:15:06 -0400

    code maintenance

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,25 @@
 import React from 'react';
 
-import { ChakraProvider, Box, Flex, Text, Heading } from '@chakra-ui/react'
+import { ChakraProvider, Box, VStack, Text, Heading } from '@chakra-ui/react'
 
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className = 'App'>
     <ChakraProvider>
-    <Box padding = '3px'>
-    <Heading>react-infinite-grid-scroller (RIGS) demo site</Heading>
-    <Flex>
-      <Box>
-      <Text>
-        Open the right drawer for options. Open the left drawer for explanations. 
-      </Text>
-      <Text>
-        On the desktop, change the browser size to change the viewport size.
-      </Text>
+      <Box padding = {[1,3]}>
+        <Heading fontSize = {[20,20,30]} mb = {2}>react-infinite-grid-scroller (RIGS) demo site</Heading>
+        <VStack align = 'start'>
+          <Text>
+            Open the right drawer for options. Open the left drawer for explanations. 
+          </Text>
+          <Text>
+            On the desktop, change the browser size to change the viewport size.
+          </Text>
+        </VStack>
       </Box>
-    </Flex>
-    </Box>
     </ChakraProvider>
-    </div>
   );
 }
 
