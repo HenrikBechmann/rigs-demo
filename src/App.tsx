d09e03cb3c3d9ab72244826a4ff1ee861135869a
Henrik Bechmann <henrik@therefore.ca>
2022-10-16 07:57:59 -0400

    drawers

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
-import { ChakraProvider, Box, HStack, Grid, Heading, Button } from '@chakra-ui/react'
+import { 
+  ChakraProvider, 
+  Box, 
+  HStack, 
+  Grid, 
+  Heading, 
+  Button, 
+  useDisclosure,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+} from '@chakra-ui/react'
 
 function App() {
+  const { isOpen:isOpenOptions, onOpen:onOpenOptions, onClose:onCloseOptions } = useDisclosure()
+  const { isOpen:isOpenExplanations, onOpen:onOpenExplanations, onClose:onCloseExplanations } = useDisclosure()
+  const optionsButtonRef = React.useRef(null)
+  const explanationsButtonRef = React.useRef(null)
+
   return (
     <ChakraProvider>
     <Box height = '100vh'>
@@ -10,13 +29,51 @@ function App() {
       <Box padding = {[1,1,2]}>
         <Heading fontSize = {[20,20,30]} mb = {[1,1,2]}>react-infinite-grid-scroller (RIGS) demo</Heading>
         <HStack align = 'start'>
-          <Button size = {['sm','sm','md']}>Explanations</Button><Button size = {['sm','sm','md']}>Options</Button>
+          <Button ref = {explanationsButtonRef} size = {['sm','sm','md']} onClick = {onOpenExplanations}>
+            Explanations
+          </Button>
+          <Button ref = {optionsButtonRef} size = {['sm','sm','md']} onClick = {onOpenOptions}>
+            Options
+          </Button>
         </HStack>
       </Box>
       <Box margin = {[1,2,3]} border = '1px' >
       </Box>
       </Grid>
     </Box>
+    <Drawer
+      isOpen={isOpenOptions}
+      placement='right'
+      onClose={onCloseOptions}
+      finalFocusRef={optionsButtonRef}
+    >
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader>Select Scroller Options</DrawerHeader>
+
+        <DrawerBody>
+        </DrawerBody>
+
+      </DrawerContent>
+    </Drawer>
+    <Drawer
+      isOpen={isOpenExplanations}
+      placement='left'
+      size = 'md'
+      onClose={onCloseExplanations}
+      finalFocusRef={explanationsButtonRef}
+    >
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader>Explanations</DrawerHeader>
+
+        <DrawerBody>
+        </DrawerBody>
+
+      </DrawerContent>
+    </Drawer>
     </ChakraProvider>
   );
 }
