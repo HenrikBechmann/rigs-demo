c0d0f0cb4f70d91ea365e6b98969d5d727e2ced4
Henrik Bechmann <henrik@therefore.ca>
2022-10-16 07:25:50 -0400

    buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
 
-import { ChakraProvider, Box, VStack, Grid, Text, Heading } from '@chakra-ui/react'
-
-// import logo from './logo.svg';
-// import './App.css';
+import { ChakraProvider, Box, HStack, Grid, Heading, Button } from '@chakra-ui/react'
 
 function App() {
   return (
     <ChakraProvider>
     <Box height = '100vh'>
     <Grid height = '100%' autoFlow = 'row' autoRows = 'max-content 1fr'>
-      <Box padding = {[1,2,3]}>
+      <Box padding = {[1,1,2]}>
         <Heading fontSize = {[20,20,30]} mb = {[1,1,2]}>react-infinite-grid-scroller (RIGS) demo</Heading>
-        <VStack align = 'start'>
-          <Text>
-            Open the right drawer for options. Open the left drawer for explanations. 
-          </Text>
-          <Text>
-            On the desktop, change the browser size to change the viewport size.
-          </Text>
-        </VStack>
+        <HStack align = 'start'>
+          <Button size = {['sm','sm','md']}>Explanations</Button><Button size = {['sm','sm','md']}>Options</Button>
+        </HStack>
       </Box>
-      <Box margin = {[1,2,3]}>
-        Test
+      <Box margin = {[1,2,3]} border = '1px' >
       </Box>
       </Grid>
     </Box>
