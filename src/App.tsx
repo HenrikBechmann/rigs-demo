4534eb498ae88f7aca3d6fdccedf73d1ce54cbfd
Henrik Bechmann <henrik@therefore.ca>
2022-10-15 20:44:48 -0400

    grid for main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { ChakraProvider, Box, VStack, Text, Heading } from '@chakra-ui/react'
+import { ChakraProvider, Box, VStack, Grid, Text, Heading } from '@chakra-ui/react'
 
 // import logo from './logo.svg';
 // import './App.css';
@@ -8,8 +8,10 @@ import { ChakraProvider, Box, VStack, Text, Heading } from '@chakra-ui/react'
 function App() {
   return (
     <ChakraProvider>
-      <Box padding = {[1,3]}>
-        <Heading fontSize = {[20,20,30]} mb = {2}>react-infinite-grid-scroller (RIGS) demo site</Heading>
+    <Box height = '100vh'>
+    <Grid height = '100%' autoFlow = 'row' autoRows = 'max-content 1fr'>
+      <Box padding = {[1,2,3]}>
+        <Heading fontSize = {[20,20,30]} mb = {[1,1,2]}>react-infinite-grid-scroller (RIGS) demo</Heading>
         <VStack align = 'start'>
           <Text>
             Open the right drawer for options. Open the left drawer for explanations. 
@@ -19,6 +21,11 @@ function App() {
           </Text>
         </VStack>
       </Box>
+      <Box margin = {[1,2,3]}>
+        Test
+      </Box>
+      </Grid>
+    </Box>
     </ChakraProvider>
   );
 }
