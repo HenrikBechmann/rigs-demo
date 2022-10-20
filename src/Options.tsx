7ee0024509b673b08a5bc65dc5a56cf479c63db3
Henrik Bechmann <henrik@therefore.ca>
2022-10-19 21:45:17 -0400

    notes

--- src/Options.tsx
+++ src/Options.tsx
@@ -2,7 +2,8 @@
 /*
 
     - modify enabler onChange calls to verify related data, and turn off errors for unrelated data
-    - border color to signify changed value
+    - border color to signify changed value, and error value
+    - orientation changes before apply is called, should wait until after
 
 */
 
