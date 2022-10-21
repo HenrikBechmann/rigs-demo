96ace7c5f1f21608e12c430667909314e7a78fcf
Henrik Bechmann <henrik@therefore.ca>
2022-10-21 01:34:33 -0400

    code maintenance

--- src/Options.tsx
+++ src/Options.tsx
@@ -37,7 +37,7 @@ const exists = (value:string) => {
 }
 
 const minValue = (value:any, minValue:number) => {
-    const testvalue = Number(value)
+    const testvalue = +value
     const test = (!isNaN(testvalue) && testvalue >=minValue )
     return test
 }
