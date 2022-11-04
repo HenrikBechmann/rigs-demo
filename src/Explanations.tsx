d9c5d5d11568e5932fea4f3581283fe28b1b516c
Henrik Bechmann <henrik@therefore.ca>
2022-11-04 16:07:17 -0400

    establish markdown

--- src/Explanations.tsx
+++ src/Explanations.tsx
@@ -4,9 +4,28 @@ import React from 'react'
 
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import 'github-markdown-css'
 
+import {Box} from '@chakra-ui/react'
+
+const introduction = `
+There are two reasons for this website:
+
+- to provide a way to become familiar with the  features of _*react-infinite-grid-scroller*_ (**RIGS**)
+- to provide a test environment for various platforms. If you see any issues, please report them 
+[here](https://github.com/HenrikBechmann/react-infinite-grid-scroller/issues)
+
+There are two ways to experiment with content sizing and configuration on a desktop device:
+
+- change the size of the browser window
+- zoom the browser window (Ctrl-minus or Ctrl-plus). Zooming down to 33% is interesting
+
+`
 const Explanations = (props:any) => {
-    return null
+
+    return <Box className = 'markdown-body' fontSize = 'sm'>
+        <ReactMarkdown children = {introduction} remarkPlugins = {[remarkGfm]} />
+    </Box>
 }
 
 export default Explanations
