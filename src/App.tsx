0fb7cbcb16a1fb4e30b8a7d632c7ab7a7d3d20c4
Henrik Bechmann <henrik@therefore.ca>
2023-08-21 19:29:52 -0400

    udpate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-// copyright (c) 2022 Henrik Bechmann, Toronto, Licence: MIT
+// copyright (c) 2022 Henrik Bechmann, Toronto, Licence: MIT 
 
 import React, {useState, useEffect, useRef} from 'react';
 
