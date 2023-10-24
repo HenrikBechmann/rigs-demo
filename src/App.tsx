e8c6ca59b189ed5fb23adff4d19e71ff5fc020b2
Henrik Bechmann <henrik@therefore.ca>
2023-10-24 17:24:43 -0400

    test config

--- src/App.tsx
+++ src/App.tsx
@@ -255,7 +255,7 @@ function App() {
   },[])
 
   // baseline - static
-  const defaultContentTypeSelector = 'uniformcontent' // 'nestinguniform'
+  const defaultContentTypeSelector = 'variablecontent' //'uniformcontent' // 'nestinguniform'
   const defaultOperationFunctionSelector = ''
   // defaultAllContentTypeProperties imported above
   // defaultCallbackFlags imported above
